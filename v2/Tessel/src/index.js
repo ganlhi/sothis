0b51a3a2ea14c3f97c23ba0401cd05d6b56c1c21
Guilhem Brouat <guilhem.brouat@docdoku.com>
2017-01-18 09:24:25 +0100

    Begin WS with diet.js

--- v2/Tessel/src/index.js
+++ v2/Tessel/src/index.js
@@ -1,15 +1,66 @@
 'use strict'
 
-const Leds = require('./lib/leds')
-const Buttons = require('./lib/buttons')
+// const Leds = require('./lib/leds')
+// const Buttons = require('./lib/buttons')
 
-const leds = new Leds()
-const buttons = new Buttons()
+// const leds = new Leds()
+// const buttons = new Buttons()
 
-buttons.on('push', name => {
-  console.log(`Push ${name}`)
-  leds.toggleState(name)
+// buttons.on('push', name => {
+//   console.log(`Push ${name}`)
+//   leds.toggleState(name)
+// })
+
+// setInterval(() => {}, 1000)
+
+const server = require('diet')
+const app = server()
+app.listen('http://localhost:8000')
+
+let states = {
+  mount: 1,
+  dslr: 0,
+  roofOpen: 0,
+  roofClose: 0
+}
+
+app.get('/:name', function($) {
+  if (!states.hasOwnProperty($.params.name)) {
+    $.failure()
+  } else {
+    let data = {}
+    data[$.params.name] = states[$.params.name]
+    $.json(data)
+  }
 })
 
-setInterval(() => {}, 1000)
+app.post('/roof/:action', function($) {
+  if ($.params.action === 'open') {
+    states.roofClose = 0
+    states.roofOpen = 1
+    setTimeout(() => {
+      states.roofOpen = 0
+      $.json({ roof: 'open' })
+    }, 10000)
+  } else if ($.params.action === 'close') {
+    states.roofClose = 1
+    states.roofOpen = 0
+    setTimeout(() => {
+      states.roofClose = 0
+      $.json({ roof: 'close' })
+    }, 10000)
+  } else {
+    $.failure()
+  }
+})
 
+app.post('/:name/:state', function($) {
+  if (!states.hasOwnProperty($.params.name) || ($.params.state != 1 && $.params.state != 0)) {
+    $.failure()
+  } else {
+    states[$.params.name] = $.params.state
+    let data = {}
+    data[$.params.name] = $.params.state
+    $.json(data)
+  }
+})
