4bcc370c82fa947274a6a57c587f241898a346ea
Guilhem Brouat <Guilhem Brouat>
2017-03-16 19:21:09 +0100

    Removed old stuff and moved good stuff up

--- scripts/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  url: 'ws://192.168.0.42:1880/api/socket'
-} 
--- scripts/every.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function every(obj, value) {
-  let ok = true;
-  for (let k in obj) {
-    ok = ok && obj[k] === value;
-  }
-  return ok;
-}
